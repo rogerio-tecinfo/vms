4517f8be6f6b3d0547680d23f0132eac677da44e
Rogerio Prazeres <rogerio.prazeres@outlook.com.br>
2023-01-16 16:22:42 -0300

    ergsdfy54th

--- Linux/provider.tf
+++ Linux/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.37.0"
+    }
+  }
+}
 provider "azurerm" {
- features {}
+  features {}
 }
