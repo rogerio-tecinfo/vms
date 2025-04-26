a460213a692180f355c112cff052af81dbe1b759
rogerio prazeres <rogerio.prazeres@outlook.com.br>
2025-04-26 10:36:32 -0300

    update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,10 @@
 terraform {
-  backend "azurerm" {}
+  required_version = ">= 1.3.7"
+
+  backend "azurerm" {
+    resource_group_name  = "RG02"                    # Nome do Resource Group que tem o Storage Account
+    storage_account_name = "restorevmrg02"           # Nome do Storage Account (sem caixa alta)
+    container_name       = "tfstate"                  # Nome do container no Blob Storage
+    key                  = "prod.tfstate"             # Nome do arquivo .tfstate que será salvo
+  }
 }
